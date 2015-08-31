a8edae4b5334936212585636fe569f98cc8908b7
shenzhan <842429012@qq.com>
2015-08-31 17:36:45 +0800

    初步的测试版

---
+++ Assets/HttpAPI/HttpAPI.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+
+/// <summary>
+/// Http Request SDK 
+/// </summary>
+namespace RestHttpAPI
+{
+public class HttpAPI : MonoBehaviour {
+    
+		private static HttpAPI _instacne=null;
+		private HttpAPI()
+		{
+		}
+
+		public static HttpAPI Instance{
+			get{
+				if(_instacne == null )
+				{
+					Debug.LogError("Awake error");
+				}
+				return _instacne ;
+			}
+		}
+
+		void Awake () {
+			DontDestroyOnLoad (gameObject);
+			HttpAPI._instacne = gameObject.GetComponent<HttpAPI> ();   
+				
+			JsonDic.Add("Content-Type", "application/json");
+
+			AuthDic.Add("Content-Type", "application/json");
+			string NameAndPw = UserName + ":" + Password;
+			AuthDic.Add("Authorization", "Basic " + System.Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(NameAndPw)) );
+
+		}
+
+		public string ipAdr="https://127.0.0.1:5000";
+		public string ver = "";
+		Dictionary<string,string> AuthDic = new Dictionary<string,string> ();  // auth header
+		Dictionary<string,string> JsonDic = new Dictionary<string,string> ();  // json parser header
+
+		public string UserName="";
+		public string Password="";
+
+
+		/// <summary>
+		/// Get Method with callback
+		/// </summary>
+		/// <param name="url">URL.</param>
+		/// <param name="callBack">Call back.</param>
+		public void Get(string url,Action<string,string> callBack=null)
+		{
+			StartCoroutine ( IEHttpGet(this.ipAdr+url,callBack));
+		}
+		IEnumerator IEHttpGet(string fullUrl,Action<string,string> callBack)
+		{
+			//print ("get url " + fullUrl);
+			WWW www = new WWW(fullUrl);
+			yield return www;
+			
+			if (www.error != null) {
+				Debug.LogError (www.error);
+			} 
+
+			if(callBack != null)
+			   callBack (www.text,www.error);
+		}
+
+		/// <summary>
+		/// Get Method with callback and  BaseAuth
+		/// </summary>
+		/// <param name="url">URL.</param>
+		/// <param name="callBack">Call back.</param>
+		public void GetAuth(string url,Action<string,string> callBack=null)
+		{
+			StartCoroutine (IEGetAuth(url,callBack));
+		}
+		IEnumerator IEGetAuth(string url,Action<string,string> callBack=null)
+		{
+
+			string fullUrl = this.ipAdr + url;
+
+
+			WWW www = new WWW(fullUrl,null,AuthDic);
+			yield return www;
+			
+			if (www.error != null) {
+				Debug.LogError ("error:"+www.error);
+			} 
+			
+			if(callBack != null)
+				callBack (www.text,www.error);
+		}
+
+		/// <summary>
+		/// Post the specified url and callBack.
+		/// </summary>
+		/// <param name="url">URL.</param>
+		/// <param name="callBack">Call back.</param>
+		public void Post(string url,Action<string,string> callBack=null)
+		{
+			StartCoroutine (IEPost (url, callBack));
+		}
+		IEnumerator IEPost(string url,Action<string,string> callBack=null)
+		{
+			string fullUrl = this.ipAdr + url;
+
+			WWWForm wf = new WWWForm ();
+			wf.AddField ("K","v");
+			
+			WWW www = new WWW(fullUrl,wf);
+			yield return www;
+			
+			if (www.error != null) {
+				Debug.LogError ("error:"+www.error);
+			}
+			
+			if(callBack != null)
+				callBack (www.text,www.error);
+		}
+
+		/// <summary>
+		/// Post the specified url, data and callBack.
+		/// </summary>
+		/// <param name="url">URL.</param>
+		/// <param name="data">Data.</param>
+		/// <param name="callBack">Call back.</param>
+		public void Post(string url,string data, Action<string,string> callBack=null)
+		{
+			StartCoroutine (IEPost(url,data,callBack));
+		}
+		IEnumerator IEPost(string url,string data, Action<string,string> callBack=null)
+		{
+			string fullUrl = this.ipAdr + url;
+			
+			byte[] post_data;  
+			post_data = System.Text.UTF8Encoding.UTF8.GetBytes(data);  
+			
+			WWW www = new WWW(fullUrl,post_data,JsonDic);
+			yield return www;
+			
+			if (www.error != null) {
+				Debug.LogError ("error:"+www.error);
+			} 
+			if(callBack != null)
+				callBack (www.text,www.error);
+		}
+
+
+		/// <summary>
+		/// Posts the auth.
+		/// </summary>
+		/// <param name="url">URL.</param>
+		/// <param name="data">Data.</param>
+		/// <param name="callBack">Call back.</param>
+		public void PostAuth(string url,string data, Action<string,string> callBack=null)
+		{
+			StartCoroutine (IEPostAuth(url,data,callBack));
+		}
+		IEnumerator IEPostAuth(string url,string data, Action<string,string> callBack=null)
+		{
+			string fullUrl = this.ipAdr + url;
+			
+			byte[] post_data;  
+			post_data = System.Text.UTF8Encoding.UTF8.GetBytes(data);  
+			
+			WWW www = new WWW(fullUrl,post_data,AuthDic);
+			yield return www;
+			
+			if (www.error != null) {
+				Debug.LogError ("error:"+www.error);
+			}
+			
+			if(callBack != null)
+				callBack (www.text,www.error);
+		}
+	
+}
+
+}
---
+++ Assets/reqTest.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using RestHttpAPI;
+using MiniJSON;
+
+public class reqTest : MonoBehaviour {
+
+
+
+	string url = "https://127.0.0.1:5000/user/newUser";
+	public string postData="";
+
+	public void TestAPI()
+	{   
+		//print ("TestAPI");
+		//StartCoroutine ("IETestAPI");
+		//StartCoroutine ("IEAuth");
+
+		//ActionTest ( ()=>{ print(url);} );
+		HttpAPI.Instance.ipAdr = "https://127.0.0.1:5000";
+//		HttpAPI.Instance.UserName = "b";
+//		HttpAPI.Instance.Password = "123";
+		//HttpAPI.Instance.Get ("/hello",(resp,error)=>{print(resp+" "+error);});
+		//HttpAPI.Instance.Post ("/hello",(resp,error)=>{print(resp+" "+error);});
+		//HttpAPI.Instance.GetAuth ("/test",(resp,error)=>{print(resp+" "+error);});
+
+		//new user 
+//		Dictionary<string,string> UserDic = new Dictionary<string, string> ();
+//		UserDic["username"]="you";
+//		UserDic["password"]="123";
+//		string str = Json.Serialize(UserDic);
+//		HttpAPI.Instance.Post ("/user/newUser", str, (resp,error) => {
+//			print (resp + " " + error);}
+//		);
+
+		//check code
+		Dictionary<string,string> UserDic = new Dictionary<string, string> ();
+		UserDic["appname"]="aote2";
+		UserDic["channel"]="haha";
+		UserDic["exCode"]="SGUAYCNYRB";
+		string str = Json.Serialize(UserDic);
+		HttpAPI.Instance.Post("/checkCode", str, (resp,error) => {
+			print (resp + " " + error);}
+		);
+
+		//generateCode
+//		Dictionary<string,string> UserDic = new Dictionary<string, string> ();
+//		UserDic["appname"]="aote2";
+//		UserDic["channel"]="haha";
+//		UserDic["count"]="180";
+//		string str = Json.Serialize(UserDic);
+//		HttpAPI.Instance.PostAuth("/generateCode", str, (resp,error) => {
+//			print (resp + " " + error);}
+//		);
+
+
+		//get apps
+//		HttpAPI.Instance.GetAuth ("/getexcodeinfo", (resp,error) => {
+//			print (resp + " " + error);
+//		});
+	}
+
+
+
+
+	void ActionTest(Action callback)
+	{
+		callback ();
+	}
+
+	IEnumerator IETestAPI()
+	{
+		byte[] post_data;  
+		Dictionary<string,string> dic = new Dictionary<string,string> ();
+
+		string str_params="";  
+		str_params ="{\"username\":\"c\",\"password\":\"123\"}";   
+		print (str_params);
+		post_data = System.Text.UTF8Encoding.UTF8.GetBytes(str_params);  
+
+//		WWWForm	form  = new WWWForm();
+//		form.AddField("u","A");
+
+		dic.Add("Content-Type", "application/json");
+
+		WWW www = new WWW(url,post_data,dic);
+		yield return www;
+
+		if (www.error != null) {
+			print (www.error);
+		} else {
+			print ("test"+www.text);
+		}
+
+
+	}
+
+	IEnumerator IEAuth()
+	{
+		string url = "https://127.0.0.1:5000/test";
+
+
+		Dictionary<string,string> dic = new Dictionary<string,string> ();
+		dic.Add("Content-Type", "application/json");
+		dic.Add("Authorization", "Basic " + System.Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes("b:123")) );
+		
+		WWW www = new WWW(url,null,dic);
+		yield return www;
+		
+		if (www.error != null) {
+			print (www.error);
+		} else {
+			print ("test"+www.text);
+		}
+
+	}
+}
